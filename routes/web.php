bed2d4c198619711f38ee9d4b9c83c298af28207
revin akmal <revinakmal172@gmail.com>
2024-07-09 10:39:00 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\testingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/testing', function () {
+    return 'hello word';
+});
+
+Route::get('testing',[testingController::class, 'testing'])->middleware('token');
+
